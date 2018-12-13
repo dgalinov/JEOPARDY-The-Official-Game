962b1c875e885c99a82094bdee62816336c97dff
Renzo Luna <renzo.luna.liendo@gmail.com>
2018-12-13 16:02:07 +0100

    Haiwemfoiawmef

--- JEOPARDY/src/fileAction.java
+++ JEOPARDY/src/fileAction.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class fileAction {
     private final String filePath;
-    private final Question[][] question = new Question[6][5];
+    private final Question[][] question = new Question[5][6];
     private final String[] topic = new String[6];
 
     public fileAction(String filePath) {
@@ -16,18 +16,25 @@ public class fileAction {
         String line;
         int i = 0;
         int j = 0;
+        int x = 0;
+        int z = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             while ((line = reader.readLine()) != null) {
-                if (j == 0){
-                    topic[i] = line;
+                if (z == 0){
+                    topic[x] = line;
                 } else {
                     String[] parts = line.split(";", 2);
                     Question q = new Question(parts[0], parts[1]);
-                    question[i][j - 1] = q;
+                    question[i][j] = q;
+                    i++;
+                }
+                z++;
+                if (z == 5){
+                    z = 0;
+                    x++;
                 }
 
-                i++;
-                if(i == 6){
+                if(i == 5){
                     i = 0;
                     j++;
                 }
