4c31b85e74814ad1a3e4976cf41e8e6b66d9dc56
Dragomir <d.galinov.d.salle@gmail.com>
2018-12-13 15:53:45 +0100

    AWEFAWEF

---
+++ JEOPARDY/src/JeopardyView.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class JeopardyView {
+    static JTextArea titleMenu = new JTextArea("JEOPARDY");
+
+    public void main(String[] args) {
+
+    }
+    private static void ShowMainMenu() {
+        JFrame frameMainMenu = new JFrame();
+    }
+    private static void ShowInstructions() {
+        JFrame frameInstructions = new JFrame();
+    }
+    private static void ShowSetNameMenu() {
+        JFrame frameSetNames = new JFrame();
+    }
+    private static void ShowMainGame() {
+        JFrame frameGame = new JFrame();
+    }
+    private static void ShowEndGame() {
+        JFrame frameCredits = new JFrame();
+    }
+}
