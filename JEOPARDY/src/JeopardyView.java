b4bc6570e1c2b0b89e083fe90acbaa4185419d18
Dragomir <d.galinov.d.salle@gmail.com>
2018-12-23 14:55:41 +0100

    auiwneiunaouiwe

--- JEOPARDY/src/JeopardyView.java
+++ JEOPARDY/src/JeopardyView.java
@@ -2,49 +2,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JeopardyView {
-    public static void main(String[] args) { ShowMainMenu();}
+    public static void main(String[] args) {}
 
-    private static void ShowMainMenu() {
-        JFrame frameMainMenu = new JFrame("Jeopardy");
-        addPanel(frameMainMenu.getContentPane());
-        frameMainMenu.setSize(1250,750);
-        frameMainMenu.setResizable(false);
-        frameMainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frameMainMenu.setVisible(true);
-    }
-    private static void addPanel(Container contentPane) {
-        JPanel topicButtons = new JPanel();
-        topicButtons.setLayout(new GridBagLayout());
-
-        JLabel topicButton1 = new JLabel("JUAN");
-        topicButton1.setForeground(Color.green);
-        JLabel topicButton2 = new JLabel("ES");
-        topicButton2.setForeground(Color.red);
-        JLabel topicButton3 = new JLabel("MARICA");
-        topicButton3.setForeground(Color.blue);
-        JLabel topicButton4 = new JLabel("!");
-        topicButton4.setForeground(Color.pink);
-        JLabel topicButton5 = new JLabel("MARC");
-        topicButton5.setForeground(Color.orange);
-        JLabel topicButton6 = new JLabel("GAY");
-        topicButton6.setForeground(Color.magenta);
-
-        topicModelText(topicButtons, "", 1, topicButton1);
-        topicModelText(topicButtons, "", 2, topicButton2);
-        topicModelText(topicButtons, "", 3, topicButton3);
-        topicModelText(topicButtons, "", 4, topicButton4);
-        topicModelText(topicButtons, "", 5, topicButton5);
-        topicModelText(topicButtons, "", 6, topicButton6);
-
-        contentPane.add(topicButtons, BorderLayout.PAGE_START);
-    }
-    private static void topicModelText(JPanel topicButtons, String label, int x, JLabel topicButtonCreation) {
+    public static void ShowGame() {
+        JFrame frame = new JFrame();
+        JLabel[][] questions = new JLabel[5][6];
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        JLabel labelModel = new JLabel(label);
-        gbc.gridx = x;
-        gbc.gridy = 0;
-        topicButtons.add(labelModel, gbc);
-        gbc.gridy = 1;
-        topicButtons.add(topicButtonCreation, gbc);
+
+
+        for(int i = 0; i < questions.length; i++) {
+            for(int j = 0; j < questions[i].length;j++) {
+                int money = ((j + 1)+100);
+                JLabel label = new JLabel(String.valueOf(money));
+                gbc.gridx = i;
+                gbc.gridy = j + 1;
+                panel.add(label, gbc);
+                questions[i][j] = label;
+            }
+        }
     }
 }
