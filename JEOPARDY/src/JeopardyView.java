a93d8b7a245b9ee2fd16930992ec8a03b97ad60f
Renzo Luna <renzo.luna.liendo@gmail.com>
2019-01-10 16:42:01 +0100

    ifjaniniurvise

--- JEOPARDY/src/JeopardyView.java
+++ JEOPARDY/src/JeopardyView.java
@@ -2,25 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JeopardyView {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
 
-    public static void ShowGame() {
-        JFrame frame = new JFrame();
-        JLabel[][] questions = new JLabel[5][6];
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
+        CreateAndShowGUI();
+    }
 
+    private static void CreateAndShowGUI(){
+        JFrame frame = new JFrame("Add user");
 
-        for(int i = 0; i < questions.length; i++) {
-            for(int j = 0; j < questions[i].length;j++) {
-                int money = ((j + 1)+100);
-                JLabel label = new JLabel(String.valueOf(money));
-                gbc.gridx = i;
-                gbc.gridy = j + 1;
-                panel.add(label, gbc);
-                questions[i][j] = label;
-            }
-        }
+        addComponentsToPane(frame.getContentPane());
+
+        frame.setSize(1000,500);
+
+        frame.setResizable(false);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+
+    private static void addComponentsToPane(Container pane){
+
     }
 }
